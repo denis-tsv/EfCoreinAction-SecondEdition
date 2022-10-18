4c29d8d8fde738669ed5edad21ded9eb784e716f
Denis <yadeniskotcvet@gmail.com>
2022-10-18 19:38:52 +0700

    UseCases

--- ServiceLayer/BookServices/BookListCombinedDto.cs
+++ ServiceLayer/BookServices/BookListCombinedDto.cs
@@ -2,7 +2,6 @@
 // Licensed under MIT license. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using ServiceLayer.Logger;
 
 namespace ServiceLayer.BookServices
 {
